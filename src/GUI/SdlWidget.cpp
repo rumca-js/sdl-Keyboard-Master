a106c636874d56167189666cbecebdbfea42a48c
X Y Z <rozbujnik@gmail.com>
2021-07-05 13:19:19 +0200

    Added widget class -a

---
+++ src/GUI/SdlWidget.cpp
@@ -0,0 +1,8 @@
+#ifndef __H_SDL_WIDGET_H_
+#define __H_SDL_WIDGET_H_
+
+void SdlWidget::init();
+void SdlWidget::draw();
+void SdlWidget::close();
+
+#endif
---
+++ src/GUI/SdlWidget.h
@@ -0,0 +1,11 @@
+#ifndef __H_SDL_WIDGET_H_
+#define __H_SDL_WIDGET_H_
+
+class SdlWidget
+{
+    void init();
+    void draw();
+    void close();
+};
+
+#endif
