2432ca659d854fb29b0a3e7dcd5557dc8e8e4eef
X Y Z <rozbujnik@gmail.com>
2021-07-05 13:19:24 +0200

    Added widget class

--- src/GUI/SdlWidget.h
+++ src/GUI/SdlWidget.h
@@ -3,9 +3,34 @@
 
 class SdlWidget
 {
+    /*!
+     * initialize widget
+     */
     void init();
+    /*!
+     * draw widget
+     */
     void draw();
+    /*!
+     * close widget
+     */
     void close();
+    /*!
+     * sets position
+     */
+    void setPosition(int x, int y);
+    /*!
+     * sets dimensions
+     */
+    void setDimensions(int w, int h);
+    /*!
+     * relative toward screen position
+     */
+    void setPositionR(double x, double y);
+    /*!
+     * relative toward screen dimentions
+     */
+    void setDimensionsR(double w, double h);
 };
 
 #endif
