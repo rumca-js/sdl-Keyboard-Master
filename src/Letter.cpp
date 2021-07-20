a82c6bc1acd2a3fa0571bac6e23aaf01297915dc
Piotr Zielinski <rozbujnik@gmail.com>
2021-07-20 22:21:17 +0200

    Dot fix

--- src/Letter.cpp
+++ src/Letter.cpp
@@ -2,13 +2,15 @@
  * Letter.cpp
  */
 
-#include "MainConfiguration.h"
 #include "Letter.h"
+
+#include "MainConfiguration.h"
 #include "./Images/DrawLetter.h"
 #include "./Images/DrawGif.h"
+#include "Utilities.h"
 
 Letter::Letter(SDL_Renderer *ren, TTF_Font* Sans, char _letter, SDL_Color aColor) 
-	: ScreenStateElement(ren)
+    : ScreenStateElement(ren)
 {
     renderer = ren;
 
@@ -17,16 +19,19 @@ Letter::Letter(SDL_Renderer *ren, TTF_Font* Sans, char _letter, SDL_Color aColor
     unsigned int _w = MainConfiguration::getConfig().getLetterWidth();
     unsigned int _h = MainConfiguration::getConfig().getLetterHeight();
 
+    _w = rand_min_max(_w/2, _w);
+    _h = rand_min_max(_h/2, _h);
+
     if (letter == '.' || letter == ',' || letter == ';')
         _w = _w/2;
 
-	setDimensions(_w, _h);
+    setDimensions(_w, _h);
 
-	state = 0;
+    state = 0;
 
     if (Sans != NULL) {
-		items[STATE_NORMAL] = new DrawLetter(letter, ren, Sans, aColor);
-		items[STATE_DESTROYED] = new DrawGif("data/textures/explosion.gif", ren);
+        items[STATE_NORMAL] = new DrawLetter(letter, ren, Sans, aColor);
+        items[STATE_DESTROYED] = new DrawGif("data/textures/explosion.gif", ren);
     }
     else {
         printf("Could not open font");
@@ -34,8 +39,8 @@ Letter::Letter(SDL_Renderer *ren, TTF_Font* Sans, char _letter, SDL_Color aColor
 
     setPosition(0,0);
 
-	forceX = 0;
-	forceY = 0;
+    forceX = 0;
+    forceY = 0;
 }
 
 Letter::~Letter() {
@@ -53,16 +58,16 @@ void Letter::move() {
 }
 
 bool Letter::setDestroyed() {
-	DrawGif * gif = (DrawGif *)items[STATE_DESTROYED];
-	gif->resetAnimation();
-	return setState(STATE_DESTROYED);
+    DrawGif * gif = (DrawGif *)items[STATE_DESTROYED];
+    gif->resetAnimation();
+    return setState(STATE_DESTROYED);
 }
 
 bool Letter::isRemovable() {
-	DrawGif * gif = (DrawGif *)items[STATE_DESTROYED];
+    DrawGif * gif = (DrawGif *)items[STATE_DESTROYED];
 
-	return getState() == STATE_DESTROYED &&
-		gif->getCycles() > 0;
+    return getState() == STATE_DESTROYED &&
+           gif->getCycles() > 0;
 }
 
 void Letter::setForce(int aforceX, int aforceY) {
